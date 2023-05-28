328ead86d427f998b5aed2b5b08bf69212a66647
olehmartynenko <martynenkooleg77@gmail.com>
2023-05-28 12:44:40 +0300

    Add operator detecting functions

--- logic/regex.py
+++ logic/regex.py
@@ -1,1 +1,22 @@
-# There will be implemented regex functions
+def is_start(char):
+    return char == '^'
+
+
+def is_end(char):
+    return char == '$'
+
+
+def is_star(char):
+    return char == '*'
+
+
+def is_plus(char):
+    return char == '+'
+
+
+def is_question(char):
+    return char == '?'
+
+
+def is_operator(char):
+    return is_star(char) or is_plus(char) or is_question(char)
