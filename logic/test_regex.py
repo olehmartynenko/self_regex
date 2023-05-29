238a0ebda4363d4580a4728747f32ce3655f75fa
olehmartynenko <martynenkooleg77@gmail.com>
2023-05-29 13:49:15 +0300

    Add tests for operator detecting functions

---
+++ logic/test_regex.py
@@ -0,0 +1,24 @@
+import unittest
+from regex import *
+
+class RegexMatcherTests(unittest.TestCase):
+    def test_is_start(self):
+        self.assertTrue(is_start('^'))
+        self.assertFalse(is_start('a'))
+
+    def test_is_end(self):
+        self.assertTrue(is_end('$'))
+        self.assertFalse(is_end('a'))
+
+    def test_is_star(self):
+        self.assertTrue(is_star('*'))
+        self.assertFalse(is_star('a'))
+
+    def test_is_plus(self): 
+        self.assertTrue(is_plus('+'))
+        self.assertFalse(is_plus('a'))  
+
+    def test_is_question(self):             
+        self.assertTrue(is_question('?'))
+        self.assertFalse(is_question('a'))
+        
