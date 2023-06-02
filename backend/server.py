f869e42c25974a5f0a4979b45640cf6ed3cb343d
olehmartynenko <martynenkooleg77@gmail.com>
2023-06-02 12:32:46 +0300

    Finished backend

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,28 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from logic.regex import *
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route('/replace', methods=['POST'])
+def handle_text():
+    data = request.get_json()
+    text = data['text']
+    regex = data['regex']
+    replacement = data['replace']
+    result = replace_matches(regex, text, replacement)
+    print(result)
+    return jsonify({'result': result})
+
+@app.route('/match', methods=['POST'])
+def handle_match():
+    data = request.get_json()
+    text = data['text']
+    regex = data['regex']
+    matches = match(regex, text)
+    print(matches)
+    return jsonify({'matches': matches})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8000)
